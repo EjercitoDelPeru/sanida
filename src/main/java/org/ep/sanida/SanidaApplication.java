148b64c0bb672ada0f73b8a681f08a4f1ef7848a
A. Angel Sullon <asullom@gmail.com>
2024-06-25 11:20:03 +0200

    first commit

---
+++ src/main/java/org/ep/sanida/SanidaApplication.java
@@ -0,0 +1,13 @@
+package org.ep.sanida;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SanidaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SanidaApplication.class, args);
+	}
+
+}
